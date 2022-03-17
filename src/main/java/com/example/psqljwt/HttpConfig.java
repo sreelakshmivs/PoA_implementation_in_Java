36af26ec4ec94e4e13620fb449a102183e0d1963
Nicklas Nyström <nicklas.nystrom@xarepo.com>
2022-03-17 17:56:54 +0100

    Added RestTemplate and certificates

---
+++ src/main/java/com/example/psqljwt/HttpConfig.java
@@ -0,0 +1,55 @@
+package com.example.psqljwt;
+
+import javax.net.ssl.SSLContext;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.util.ResourceUtils;
+import org.springframework.web.client.RestTemplate;
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContextBuilder;
+
+@Configuration
+public class HttpConfig {
+
+    @Value("${server.ssl.key-store}")
+    private String KEY_STORE;
+
+    @Value("${server.ssl.trust-store}")
+    private String TRUST_STORE;
+
+    @Value("${server.ssl.key-password}")
+    private String KEY_PASSWORD;
+
+    @Value("${server.ssl.key-store-password}")
+    private String KEY_STORE_PASSWORD;
+
+    @Value("${server.ssl.trust-store-password}")
+    private String TRUST_STORE_PASSWORD;
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) throws Exception {
+        final SSLContext sslContext = SSLContextBuilder
+                .create()
+                .loadKeyMaterial(
+                        ResourceUtils.getFile(KEY_STORE),
+                        KEY_STORE_PASSWORD.toCharArray(),
+                        KEY_PASSWORD.toCharArray())
+                .loadTrustMaterial(
+                        ResourceUtils.getFile(TRUST_STORE),
+                        TRUST_STORE_PASSWORD.toCharArray())
+                .build();
+
+        final HttpClient client = HttpClients.custom()
+                .setSSLContext(sslContext)
+                .build();
+
+        return builder
+                .requestFactory(() -> new HttpComponentsClientHttpRequestFactory(client))
+                .build();
+    }
+
+}
