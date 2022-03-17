1dc39ce28d8eea82787890ef168ee05a57a77193
Nicklas Nyström <nicklas.nystrom@xarepo.com>
2022-03-17 17:57:35 +0100

    Added poa-onboarding-user-request HTTP endpoint

--- src/main/java/com/example/psqljwt/resources/UserController.java
+++ src/main/java/com/example/psqljwt/resources/UserController.java
@@ -6,13 +6,16 @@ import com.example.psqljwt.services.UserService;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.client.RestTemplate;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -24,14 +27,33 @@ public class UserController {
     @Autowired
     UserService userService;
 
+    @Autowired
+    RestTemplate restTemplate;
+
+    @Value("${ah_onboarding_uri}")
+    private String AH_ONBOARDING_URI;
+
     @PostMapping("/login")
     public ResponseEntity<Map<String, String>> loginUser(@RequestBody Map<String, Object> userMap) {
         String clientId = (String) userMap.get("clientId");
         String password = (String) userMap.get("password");
         User user = userService.validateUser(clientId, password);
-        //return new ResponseEntity<>(generateJWTToken(user), HttpStatus.OK);
-
         return new ResponseEntity<>(generateJWTToken(user), HttpStatus.OK);
+    }
+
+    @GetMapping("/poa-onboarding-user-request")
+    public ResponseEntity<String> fetchOnboardingPoa() {
+        HttpEntity<String> response;
+        try {
+            response = restTemplate.getForEntity(AH_ONBOARDING_URI, String.class);
+        } catch (Exception ex) {
+            System.out.println(ex);
+            return new ResponseEntity<>(
+                    "Failed to retrieve PoA from Arrowhead PoaOnboarding controller",
+                    HttpStatus.BAD_GATEWAY);
+        }
+        System.out.println(response.getBody());
+        return new ResponseEntity<>("OK", HttpStatus.OK);
     }
 
     @PostMapping("/poa-subcontractor") //register
